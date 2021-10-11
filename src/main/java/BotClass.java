b581eb0d27ff477df3019f25fab36f951dab6ae9
tooBusyNow <OldmobileBuick54@yandex.ru>
2021-10-11 21:26:14 +0500

    Basic funcionality with API

---
+++ src/main/java/BotClass.java
@@ -0,0 +1,34 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.io.*;
+
+
+public class BotClass extends TelegramLongPollingBot {
+
+    private static final String TOKEN  = new TokenReaderClass().ReadToken();
+    private static final String BOT_NAME = "AwesomeFinancialBot";
+
+    @Override
+    public String getBotUsername () {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived (Update update) {
+        if (update.getMessage () !=null && update.getMessage ().hasText()) {
+            long chat_id = update.getMessage().getChatId();
+            try {
+                execute(new SendMessage (String.valueOf(chat_id), "Hi" + update.getMessage().getText()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

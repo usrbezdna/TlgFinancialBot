5a9accca45ee21e06a0ccc790b479366fe19e552
Daniil <49916703+sicutglacies@users.noreply.github.com>
2021-10-11 21:39:25 +0500

    updated gitignore

--- src/main/java/BotClass.java
+++ src/main/java/BotClass.java
@@ -2,7 +2,6 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import java.io.*;
 
 
 public class BotClass extends TelegramLongPollingBot {

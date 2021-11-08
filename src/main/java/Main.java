089b56cc4306f32179792a822135cddf822a4d09
tooBusyNow <81771303+tooBusyNow@users.noreply.github.com>
2021-11-09 02:39:39 +0500

    Added package field

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+package com.gamedev;
+
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;

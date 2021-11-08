4825d03fd63a3f836a77873f225395290e799ce5
tooBusyNow <81771303+tooBusyNow@users.noreply.github.com>
2021-11-09 02:43:30 +0500

    Added com.gamedev package;

--- src/main/java/BotClass.java
+++ src/main/java/BotClass.java
@@ -1,3 +1,5 @@
+package com.gamedev;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;

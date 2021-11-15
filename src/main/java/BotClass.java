01e15296d404f24f72a14a950e8c123ed230ce33
Daniil <49916703+sicutglacies@users.noreply.github.com>
2021-11-15 20:59:37 +0500

    resolved dependencies

--- src/main/java/BotClass.java
+++ src/main/java/BotClass.java
@@ -1,3 +1,5 @@
+import com.gamedev.TokenReaderClass;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;

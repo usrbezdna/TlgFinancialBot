577560bac9db3c1319ffcd79ed8b0a4f52c96d42
tooBusyNow <81771303+tooBusyNow@users.noreply.github.com>
2021-11-16 21:54:22 +0500

    Update TokenReaderClass.java

--- src/main/java/com/gamedev/TokenReaderClass.java
+++ src/main/java/com/gamedev/TokenReaderClass.java
@@ -11,6 +11,7 @@ public class TokenReaderClass {
         String tokenAsStr = null;
         try {
             tokenAsStr = System.getenv("TOKEN");
+            System.out.println("I WAS UPDATED")
         }
         catch (SecurityException e) {
             e.printStackTrace();
